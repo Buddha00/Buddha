88f9912918be3cecd2222a77d1808edeab25f8e5
Lê Văn Trung <69235532+sevenghost-2001@users.noreply.github.com>
2023-04-19 12:26:55 +0700

    index

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+define("ABSPATH", __DIR__ . "/");
+define("ABSPATH_SITE", ABSPATH . "site" . "/");
+
+define("SERVERNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DBNAME", "godashop");
+
+define("ITEM_PER_PAGE", 9);
+
+define("GOOGLE_RECAPTCHA_SITE", "6Lcj07oUAAAAALAHcj_WdDa7Vykqzui3mSA5SIoe");
+define("GOOGLE_RECAPTCHA_SECRET", "6Lcj07oUAAAAAKKbrzK72AzMqqVwxnsVTjlvx_OJ");
+
+define("SMTP_USERNAME", "trinhvantrung2k1@gmail.com");
+define("SMTP_SECRET", "howrfxnasqiuybxy");
+define("SMTP_HOST", "smtp.gmail.com");
+define("JWT_KEY", "godashop-key");
+
+define("SHOP_OWNER", "4501104259@student.hcmue.edu.vn");
+
+define("GOOGLE_CLIENT_ID", "467247032240-4k4stmd35nn0hhloqdvtg573qb6bo2pv.apps.googleusercontent.com");
+define("GOOGLE_CLIENT_SECRET", "EbrWc27MNlMK8fo30UZ2z13N");
+
+define("FACEBOOK_CLIENT_ID", "2420071878233292");
+define("FACEBOOK_CLIENT_SECRET", "42e3caad1ba37b272aa1ef67723818ca");
